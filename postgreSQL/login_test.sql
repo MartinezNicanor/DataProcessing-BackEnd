bcb084a152c49ac19ff9f110348fcb172b50cfc0
Joey Krämer <joey@dergerat2.wifi.nhlstenden.com>
2023-12-13 16:00:07 +0100

    FIX SQL causing errors

--- postgreSQL/login_test.sql
+++ postgreSQL/login_test.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE User (
+CREATE TABLE Account (
     account_id SERIAL PRIMARY KEY,
     email VARCHAR (255) NOT NULL UNIQUE,
     password VARCHAR (255) NOT NULL,
@@ -14,8 +14,24 @@ CREATE TABLE User (
     zip_code VARCHAR (10) NOT NULL,
     country_id INT NOT NULL,
     log_in_attempt_count INT,
-    invited BOOLEAN,
+    invited BOOLEAN
 );
+
+
+-- Create the check_unique_account_limit function
+CREATE OR REPLACE FUNCTION check_unique_account_limit()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (
+        SELECT COUNT(*)
+        FROM Profile
+        WHERE account_id = NEW.account_id
+    ) > 4 THEN
+        RAISE EXCEPTION 'More than 4 profiles for the same account are not allowed';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
 
 CREATE TABLE Profile (
@@ -23,25 +39,27 @@ CREATE TABLE Profile (
     account_id INT NOT NULL,
     profile_image VARCHAR(255) DEFAULT 'location/placeholder.jpg',
     profile_name VARCHAR(255),
-    profile_child BOOLEAN DEFAULT false
+    profile_child BOOLEAN DEFAULT false,
     age INT NOT NULL,
     preferences JSON NOT NULL DEFAULT '{
-        "movie": [], 
-        "series": [], 
+        "movie": [],
+        "series": [],
         "genre": [],
-        "min_age": [], 
-        "viewing_class": [], 
-        }'
-    FOREIGN KEY (account_id) REFERENCES Account (account_id) ON DELETE CASCADE,
-    
-    -- Unique constraint to ensure that account_id is unique within the first four rows
-    CONSTRAINT unique_account_limit CHECK (
-        (SELECT COUNT(*) FROM Profile WHERE account_id = NEW.account_id) <= 4
-    )
+        "min_age": [],
+        "viewing_class": []
+    }',
+    FOREIGN KEY (account_id) REFERENCES Account (account_id) ON DELETE CASCADE
 );
 
+-- Add the trigger to enforce the unique_account_limit constraint separately
+CREATE TRIGGER unique_account_limit_trigger
+BEFORE INSERT OR UPDATE
+ON Profile
+FOR EACH ROW
+EXECUTE FUNCTION check_unique_account_limit();
+
 -- Insert data into the User table
-INSERT INTO "User" (
+INSERT INTO Account (
     email, 
     password, 
     first_name, 
